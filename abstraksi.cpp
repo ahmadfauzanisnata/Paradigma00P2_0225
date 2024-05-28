e2aa51d37612db74a509dd6ebb0608cb30c83166
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-28 15:57:57 +0700

    membuat method untuk mengisi nilai

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -3,4 +3,14 @@ using namespace std;
 
 class Abstraksiklas {
     private: string x, y;
+
+    public:
+
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+   
 }
